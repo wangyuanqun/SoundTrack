5d42c5f40800487a17f267272bcbcc821e63c660
wangyuanqun <1510365904bb@gmail.com>
2026-01-28 21:13:54 +1100

    add test file

---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::string name;
+    std::cout << "Hello what is ur name ";
+    std::cin >> name;
+    std::cout << "Hello " << name << std::endl;
+    return 0;
+}
