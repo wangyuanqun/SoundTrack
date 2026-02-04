f7131ae639a52052c5cb806bc9a4192ef57d7659
Yuanqun Wang <1510365904bb@gmail.com>
2026-02-04 12:54:57 +1100

    update vector test

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-#include <fstream>
+#include <vector>
 int main() {
     std::string name;
     std::cout << "Hello what is ur name ";
     std::cin >> name;
     std::cout << "Hello " << name << std::endl;
+
+    std::vector<std::string> names = {"rex", "max", "lucy"};
+    for (const std::string& name : names) {
+        std::cout << name << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
