612fba9b34bc4271f9a9ee7cc34e7b0993f8221d
Yuanqun Wang <1510365904bb@gmail.com>
2026-02-04 14:49:05 +1100

    update main

--- main/main.cpp
+++ main/main.cpp
@@ -11,5 +11,24 @@ int main() {
         std::cout << name << " ";
     }
     std::cout << std::endl;
+
+
+    std::vector<int> nums = {1, 2, 3, 4, 5};
+    int target = 5;
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right) {
+        int index =  left + (right - left) / 2;
+        if (nums[index] == target) {
+            std::cout << "target at index " << index << std::endl;
+            break;
+        } else if (nums[index] > target) {
+            right = index - 1;
+        } else {
+            left = index + 1;
+        }
+    }
+
+    
     return 0;
 }
